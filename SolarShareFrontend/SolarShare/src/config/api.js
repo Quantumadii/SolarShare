edb8b08b57ad1a08e2d9b4bdb2db0bacad165721
ADITYA SHINDE <158829176+Quantumadii@users.noreply.github.com>
2026-04-07 16:24:37 +0530

    Update api.js

--- SolarShareFrontend/SolarShare/src/config/api.js
+++ SolarShareFrontend/SolarShare/src/config/api.js
@@ -2,6 +2,6 @@ const API_BASE_URL =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:8081"
-    : `http://${window.location.hostname}:8081`;
+    : "/api";
 
 export default API_BASE_URL;
